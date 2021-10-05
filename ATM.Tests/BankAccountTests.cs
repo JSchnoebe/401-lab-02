3c46135e5fac3886a2f814feb9dcf588e0ab426f
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-10-05 16:31:23 -0500

    added some tests and made them pass

---
+++ ATM.Tests/BankAccountTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace ATM.Tests
+{
+    public class BankAccountTests
+    {
+        [Fact]
+        public void Account_Starts_With_No_Money()
+        {
+            //Arrange
+            BankAccount ba = new BankAccount();
+
+            //Act
+            decimal result = ba.GetBalance();
+
+            //Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Deposit()
+        {
+            BankAccount ba = new BankAccount();
+            ba.Deposit(10);
+
+            decimal result = ba.GetBalance();
+
+            Assert.Equal(0, result);
+        }
+    }
+}
---
+++ ATM/BankAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ATM
+{
+    public class BankAccount
+    {
+        private decimal balance;
+
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+
+        public void Deposit(decimal amountToDeposit)
+        {
+            balance = balance + amountToDeposit;
+        }
+
+    }
+}
