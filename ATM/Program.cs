0be0c2a14d897720d9fdbf1ad646d4bcd6f71a7c
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-10-06 15:51:25 -0500

    added methods to my program.cs

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -4,9 +4,65 @@ namespace ATM
 {
     class Program
     {
+        private static readonly BankAccount account;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteWelcome();
+
+            int selected = PromptForAction();
+
+            if (selected == 1)
+            {
+                PromptAndDeposit();
+            }
+            else if (selected == 2)
+            {
+                PromptAndWithdraw();
+            }
+        }
+
+        static void WriteWelcome()
+        {
+            Console.WriteLine("Welcome to your personal ATM!");
+        }
+
+        static int PromptForAction()
+        {
+
+            Console.WriteLine("Make a selection.");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. View Balance");
+            Console.WriteLine("4. Exit");
+
+            string input = Console.ReadLine();
+
+            int numInput = Convert.ToInt32(input);
+
+            return numInput;
+        }
+        
+        
+
+        static void PromptAndDeposit()
+        {
+            Console.WriteLine("How much would you like to deposit?");
+
+            string input = Console.ReadLine();
+
+            int numInput = Convert.ToInt32(input);
+
+            account.Deposit(numInput);
+        }
+
+        static void PromptAndWithdraw()
+        {
+            Console.WriteLine("How much would you like to withdraw?");
+
+            string input = Console.ReadLine();
+
+            int numInput = Convert.ToInt32(input);
         }
     }
 }
